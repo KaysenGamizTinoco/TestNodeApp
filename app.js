0413a474d281ce47c3040caf42b2562c964637ee
Kaysen_Gamiz <kaysengamiz@gmail.com>
2023-05-03 10:30:45 -0600

    cambios

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let i = 5;
 let j = 10;
 console.log('Iniciando...');
 console.log('La suma es ' + (5+10));
+console.log(`La suma con variables es: ${i+j}`);
 
 let fecha = moment().format('LL');
 console.log(`La fecha es ${fecha}`);
